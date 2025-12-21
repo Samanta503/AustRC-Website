92a9707b41707bcee94359a1062db9d234d84a1b
arany677 <arany.cse.20220204053@aust.edu>
2025-12-21 22:34:34 +0600

    Educational page updated

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,16 @@ import { AboutPage } from './components/AboutPage';
 import { EnthusiastAcquisitionPage } from './components/EnthusiastAcquisitionPage';
 import { Footer } from './components/Footer';
 
+// Sub-Pages
+import { EventsPage } from './components/activities/EventsPage';
+import { AchievementsPage } from './components/activities/AchievementsPage';
+import { SocialActivitiesPage } from './components/activities/SocialActivitiesPage';
+import { EducationalActivitiesPage } from './components/activities/EducationalActivitiesPage';
+import { EventWebsitePage } from './components/activities/EventWebsitePage';
+
+// ✅ THIS IMPORT IS VITAL
+import { ActivityDetailPage } from './components/activities/ActivityDetailPage';
+
 function AppContent() {
   const location = useLocation();
   const isAboutPage = location.pathname === '/about';
@@ -19,7 +29,17 @@ function AppContent() {
       <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
+        
         <Route path="/activities" element={<ActivitiesPage />} />
+        <Route path="/activities/events" element={<EventsPage />} />
+        <Route path="/activities/achievements" element={<AchievementsPage />} />
+        <Route path="/activities/social-activities" element={<SocialActivitiesPage />} />
+        <Route path="/activities/educational-activities" element={<EducationalActivitiesPage />} />
+        <Route path="/activities/event-website" element={<EventWebsitePage />} />
+
+        {/* ✅ THE DYNAMIC ROUTE FOR DETAIL PAGES */}
+        <Route path="/activity/:category/:id" element={<ActivityDetailPage />} />
+
         <Route path="/about" element={<AboutPage />} />
         <Route path="/enthusiast-acquisition" element={<EnthusiastAcquisitionPage />} />
         <Route path="/research-projects" element={<ResearchProjectsPage />} />
